8a3652a29d8b06634ca52f8fa70da4e04e81a307
wownik <wownik98@gmail.com>
2026-09-25 01:36:29 +0300

    build 1.0.2

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,4 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   outDir: 'lib', // Укажите нужную папку здесь
-  format: ['esm', 'cjs'],
-  dts: true,
-  clean: true,
 });
